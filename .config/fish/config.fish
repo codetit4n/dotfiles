1408e698bbb833c1b9cae6a329ba2c839eec01bd
Lokesh Kumar <lokeshkumar636@yahoo.com>
2023-10-24 00:30:45 +0530

    tmux, i3, some fish changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,22 @@
 function fish_greeting
-    set_color FAD5A5
-    cowsay -f tux "Hey, $USER!"
+    fortune -s | lolcat -a -s 100
 end
+
+if test -f /home/lokesh/.autojump/share/autojump/autojump.fish; . /home/lokesh/.autojump/share/autojump/autojump.fish; end
 
 alias nv "nvim"
 alias fishy "nvim ~/.config/fish/config.fish"
+alias fishyt "tmux new -s \"fish-config\" 'nvim ~/.config/fish/config.fish'"
 alias tmuxy "nvim ~/.tmux.conf"
+alias tmuxyt "tmux new -s \"tmux-config\" 'nvim ~/.tmux.conf'"
 alias starshipy "nvim ~/.config/starship.toml"
-alias terminaly "nvim ~/.alacritty.yml"
+alias starshipyt "tmux new -s \"starship-config\" 'nvim ~/.config/starship.toml'"
+alias alacritty "nvim ~/.alacritty.yml"
+alias terminalyt "tmux new -s \"alacritty-config\" 'nvim ~/.alacritty.yml'"
 alias i3y "nvim ~/.config/i3/config"
+alias i3yt "tmux new -s \"i3-config\" 'nvim ~/.config/i3/config'"
 alias nvimy "nvim ~/.config/nvim"
+alias nvimyt "tmux new -s \"nvim-config\" 'nvim ~/.config/nvim'"
 alias dotfiles "cd ~/dotfiles"
 alias dev "cd ~/dev"
 alias lync "cd ~/lync"
@@ -24,5 +31,13 @@ alias tnv "tmux new -s \"\$(basename \"\$(pwd)\" | sed 's#.*/##')\" 'nvim .'"
 
 set -gx EDITOR /usr/bin/nvim
 set -gx GIT_EDITOR /usr/bin/nvim
+
+abbr ls 'exa'
+abbr l 'exa'
+abbr ll 'exa -l'
+abbr la 'exa -la'
+
+abbr e 'exit'
+
 
 starship init fish | source
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/lokesh/.local/share/omf/themes/aight/fish_prompt.fish

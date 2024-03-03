c648a5b9d2fad2a630435a7f7fc29732478612d9
Lokesh Kumar <lokeshkumar636@yahoo.com>
2024-03-03 22:01:50 +0530

    small config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,16 @@
 function fish_greeting
     fortune -se | lolcat
+    #echo "Welcome back, Lokesh"
 end
 
 if test -f /home/lokesh/.autojump/share/autojump/autojump.fish; . /home/lokesh/.autojump/share/autojump/autojump.fish; end
 
 alias nv "nvim"
-alias fishy "tmux new -s \"fish-config\" 'nvim ~/.config/fish/config.fish; fish'"
-alias tmuxy "tmux new -s \"tmux-config\" 'nvim ~/.tmux.conf; fish'"
-alias starshipy "tmux new -s \"starship-config\" 'nvim ~/.config/starship.toml; fish'"
-alias terminaly "tmux new -s \"alacritty-config\" 'nvim ~/.alacritty.toml; fish'"
-alias i3y "tmux new -s \"i3-config\" 'nvim ~/.config/i3/config; fish'"
+alias fishy "tmux new -s \"fish-config\" 'nvim ~/.config/fish/config.fish'"
+alias tmuxy "tmux new -s \"tmux-config\" 'nvim ~/.tmux.conf'"
+alias starshipy "tmux new -s \"starship-config\" 'nvim ~/.config/starship.toml'"
+alias terminaly "tmux new -s \"alacritty-config\" 'nvim ~/.alacritty.toml'"
+alias i3y "tmux new -s \"i3-config\" 'nvim ~/.config/i3/config'"
 alias nvimy "tmux new -s \"nvim-config\" 'nvim ~/.config/nvim; fish'"
 
 alias dotfiles "cd ~/dotfiles"
@@ -46,11 +47,16 @@ abbr l 'exa'
 abbr ll 'exa -l'
 abbr la 'exa -la'
 
-abbr cat 'bat'
-
 abbr e 'exit'
 
 abbr cd 'z'
+
+abbr cat 'bat'
+abbr bat 'cat'
+
+# tmux
+abbr ta "tmux attach"
+abbr tl "tmux ls"
 
 abbr ssh-start 'sudo systemctl start sshd.service'
 abbr ssh-stop 'sudo systemctl stop sshd.service'
